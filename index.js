7090fa09997a8ae7eba33e8d3d6f01d819a0e42f
kapil-gupta-girnarsoft <kapil.gupta@girnarsoft.com>
2016-08-24 11:31:29 +0530

    updated comments and improved quality

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+/**
+ * Express controller routes Module
+ * @created  August 01, 2016
+ * @author Kapil Gupta <kapil.gp@gmail.com>
+ * @license https://raw.githubusercontent.com/kapilgp/express-controllers-routes/master/LICENSE
+ */
+
 module.exports = require('./lib/ctrlRoutes');
